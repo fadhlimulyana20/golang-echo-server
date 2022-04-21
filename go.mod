1dd6339e1b33350bc93c190742cdbb5076a52645
Fadhli Mulyana <baba.fadhli@gmail.com>
2022-04-22 03:12:47 +0700

    Added gorm

--- go.mod
+++ go.mod
@@ -16,7 +16,7 @@ require (
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
 	golang.org/x/text v0.3.7 // indirect
@@ -37,4 +37,9 @@ require (
 	github.com/joho/godotenv v1.4.0
 	go.mongodb.org/mongo-driver v1.7.3
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+)
+
+require (
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- middleware/jwt.go
+++ middleware/jwt.go
@@ -2,66 +2,52 @@ package middleware
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"strings"
 
-	"github.com/fadhlimulyana20/golang-echo-server/database"
 	"github.com/fadhlimulyana20/golang-echo-server/helper"
 	"github.com/fadhlimulyana20/golang-echo-server/model"
-	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/labstack/echo/v4"
 )
 
 var ctx = context.Background()
 
-func JwtMiddleware(c *fiber.Ctx) error {
-	authHeader := c.Get("Authorization")
-	u := new(model.User)
+func JwtMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		authorizationHeader := c.Request().Header.Get("Authorization")
 
-	if !strings.Contains(authHeader, "Bearer") {
-		data := &model.Json{
-			Status:  "failed",
-			Message: "Anda belum Login",
+		// Check Bearer
+		if !strings.Contains(authorizationHeader, "Bearer") {
+			return c.JSON(http.StatusUnauthorized, map[string]interface{}{
+				"status":  http.StatusUnauthorized,
+				"message": "Anda belum Login.",
+			})
 		}
 
-		return c.Status(http.StatusUnauthorized).JSON(data)
+		// Extract token
+		tokenString := strings.Replace(authorizationHeader, "Bearer ", "", -1)
+		j := new(helper.JWT)
+		userId, err := j.Parse(tokenString, "token")
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, &model.Json{
+				Status:  "error",
+				Message: err.Error(),
+			})
+		}
+
+		fmt.Print(userId)
+
+		// Get User by ID
+		// u := new(model.User)
+		// if err := u.Get(userId); err != nil {
+		// 	return c.JSON(http.StatusInternalServerError, &model.Json{
+		// 		Status:  "fail",
+		// 		Message: err.Error(),
+		// 	})
+		// }
+
+		return next(c)
 	}
-
-	tokenString := strings.Replace(authHeader, "Bearer ", "", -1)
-	j := new(helper.JWT)
-	userId, err := j.Parse(tokenString, "token")
-
-	if err != nil {
-		return c.Status(500).JSON(&model.Json{
-			Status:  "failed",
-			Message: err.Error(),
-		})
-	}
-
-	db := database.DbManager()
-	objectId, err := primitive.ObjectIDFromHex(userId)
-
-	if err != nil {
-		return c.Status(500).JSON(&model.Json{
-			Status:  "failed",
-			Message: err.Error(),
-		})
-	}
-
-	err = db.Collection("users").FindOne(ctx, bson.M{"_id": objectId}).Decode(&u)
-	if err == mongo.ErrNoDocuments {
-		return c.Status(404).JSON(&model.Json{
-			Status:  "failed",
-			Message: err.Error(),
-		})
-	} else if err != nil {
-		return c.Status(500).JSON(&model.Json{
-			Status:  "failed",
-			Message: err.Error(),
-		})
-	}
-
-	return c.Next()
 }
